f4da940bbccc2bcb989746408bff5ab92c346c24
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-07-18 22:44:42 +0530

    qn 6 updated

---
+++ Assingment 1/Assingment1Qn6.c
@@ -0,0 +1,16 @@
+/*WAP to print the name of the user in double quotes.
+Expected output format – “Hello , Amit Kumar”*/
+
+#include<stdio.h>
+
+int main()
+{
+   char name[20];
+
+   printf("\nEnter name : ");
+   scanf("%s", name);
+
+   printf("Hello , %s" , name);
+
+   return 0;
+}
