f29b8c860ad0014dc98fdc164a632a29d01e1b25
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-07-18 18:47:42 +0530

    qn 4 updated

---
+++ Assingment 1/Assingment1Qn4.c
@@ -0,0 +1,19 @@
+/*WAP to find the area of the circle. Take radius of circle from user as input and print the result in below given format.
+
+Expected output format – “Area of circle is A having the radius R”. Replace A with area & R with radius.*/
+
+#include<stdio.h>
+
+int main()
+{
+   float r , a , pi=3.14;
+
+   printf("\nEnter radius of the circle : ");
+   scanf("%f",&r);
+
+   a = pi*(r*r);
+
+   printf("\nArea of circle is %.2f having the radius %.2f", a,r);
+
+   return 0;
+}
