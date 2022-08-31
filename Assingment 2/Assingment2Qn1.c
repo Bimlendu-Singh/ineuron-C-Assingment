27f62ab52b7ef1d8ad8e629ffbb8f751894c0660
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-08-31 17:56:34 +0530

    Assingment 2 Qn 1 and 2 updated

---
+++ Assingment 2/Assingment2Qn1.c
@@ -0,0 +1,18 @@
+//Write a program to print unit digit of a given number
+
+#include<stdio.h>
+
+int main()
+{
+    int x,unit;
+
+    printf("Enter any number : ");
+    scanf("%d",&x);
+
+    //Storing unit digit of entered number in 'unit' variable by using modulus operator in number by 10
+    unit = x%10;
+
+    printf("Unit digit of entered number is %d ",unit);
+
+    return 0;
+}
---
+++ Assingment 2/Assingment2Qn2.c
@@ -0,0 +1,17 @@
+//Write a program to print a given number without its last digit.
+
+#include<stdio.h>
+
+int main()
+{
+    int x,num;
+
+    printf("Enter any number : ");
+    scanf("%d",&x);
+ 
+    num = x/10;
+
+    printf("Entered number without its unit digit is %d ",num);
+
+    return 0;
+}
