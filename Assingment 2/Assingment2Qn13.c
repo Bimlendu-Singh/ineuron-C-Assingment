4c923a15ecbc4e85876349e6d1dcad3e0d4c9d6e
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-09-12 15:36:39 +0530

    Assignment2 Qn13 Updated

--- Assingment 2/Assingment2Qn13.c
+++ Assingment 2/Assingment2Qn13.c
@@ -2,26 +2,18 @@
 
 #include<stdio.h>
 
+#include<stdio.h>
 int main()
 {
-    
-    int num,digit1st,digit2nd;
+    int a,x,y,newno;
 
-    printf("\nEnter the number :  ");
-    scanf("%d" , &num);
+    printf("Enter the three digit number : ");
+    scanf("%d",&a);
 
-    printf("\nOriginal number : %d", num);
+    x=(a%10)*100;
+    y=a/10;
+   newno=x+y;
+   printf("The new number is %d ",newno);
 
-    digit1st = num%10;
-    num = num/10;
-    digit2nd = num%10;
-    num = num/10;
-    num = (num*100) + (digit1st*10) + (digit2nd);
-
-    
-
-    printf("\nNumber after rotating digit : %d", num);
-   
-          
-    return 0;
+return 0;
 }
