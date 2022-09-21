47164a0f5dbf63ab3dca5fe2cc1d9005d7b39ef9
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-09-21 15:52:43 +0530

    Assingment 3 qn 9 added

--- Assingment 3/Assignment3Qn9.c
+++ Assingment 3/Assignment3Qn9.c
@@ -16,21 +16,16 @@ int main()
    printf("\nEnter third number : ");
    scanf("%d", &c);
 
-   /*if(a>b&&((b>c||b==c)||a==c))
-     printf("\nGreatest number is : %d\n", a);
-    else if(b>c&&((c>a||c==a)||b==a))
-        printf("\nGreatest number is : %d\n", b);
-     else if(c>a&&((a>b||a==b)||c==b))
-         printf("\nGreatest number is : %d\n", c);
-      else 
-         printf("\nGreatest number is : %d\n", a);  */
 
     if(a>=b&&a>=c)
       printf("\nGreatest number is : %d\n", a);
-     else if(b>=c&&b>=a)
-       printf("\nGreatest number is : %d\n", b);
-
-     
+       else if(b>=c&&b>=a)
+        printf("\nGreatest number is : %d\n", b);
+         else if(c>=a&&c>=b)
+          printf("\nGreatest number is : %d\n", c);
+           else 
+            printf("\nGreatest number is : %d\n", a);
+           
 
    return 0;
 
