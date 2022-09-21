64c4bfff1fc5398afe0e9ae8bf8a91669793f409
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-09-21 15:21:03 +0530

    Assingment 3 qn 9 added

---
+++ Assingment 3/Assignment3Qn9.c
@@ -0,0 +1,21 @@
+//9. Write a program to find the greatest among three given numbers. Print number once if the greatest number appears two or three times.
+
+#include<stdio.h>
+
+int main()
+{
+
+   int x;
+
+   printf("\nEnter a number : ");
+   scanf("%d", &x);
+
+   if(x>0)
+     printf("\nPositive\n");
+    else
+     printf("\n non-positive\n");
+     
+
+   return 0;
+
+}
