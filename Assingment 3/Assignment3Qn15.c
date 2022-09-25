d5bbfb3b863c068a45a1bd4ebd0a968eb1a4c232
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-09-25 18:54:04 +0530

    Assingment 3 Qn15 added

---
+++ Assingment 3/Assignment3Qn15.c
@@ -0,0 +1,23 @@
+//15. Write a program to check whether a given number is positive, negative or zero.
+
+#include<stdio.h>
+
+int main()
+{
+
+   int x;
+
+   printf("\nEnter a number : ");
+   scanf("%d", &x);
+
+   if(x>0)
+     printf("\nPositive\n");
+    else if(x==0)
+     printf("\nZero\n");
+     else
+      printf("\nNegative\n");
+      
+
+   return 0;
+
+}
