a462788ead767c8db987b8bb135edf1f5aec1893
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-09-26 10:00:11 +0530

    Assingment 3 Qn15 updated

--- Assingment 3/Assignment3Qn15.c
+++ Assingment 3/Assignment3Qn15.c
@@ -1,4 +1,4 @@
-//15. Write a program to check whether a given number is positive, negative or zer
+//15. Write a program to check whether a given number is positive, negative or zero.
 
 #include<stdio.h>
 
