32ff2f0da5c746ec681bdb9886e017ec6665fcf4
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-09-26 09:59:28 +0530

    Assingment 3 Qn15 updated

--- Assingment 3/Assignment3Qn15.c
+++ Assingment 3/Assignment3Qn15.c
@@ -1,4 +1,4 @@
-//15. Write a program to check whether a given number is positive, negative or z
+//15. Write a program to check whether a given number is positive, negative or ze
 
 #include<stdio.h>
 
