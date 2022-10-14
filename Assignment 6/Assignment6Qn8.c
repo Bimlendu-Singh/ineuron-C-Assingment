e2e8e66a7002ab555a4f8f819899c108ea8b5ee9
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-14 08:31:24 +0530

    updated

--- Assignment 6/Assignment6Qn8.c
+++ Assignment 6/Assignment6Qn8.c
@@ -1,1 +1,27 @@
 //8. Write a program to check whether a given number is a Prime number or not
+
+#include<stdio.h>
+
+int main()
+{
+    int x,i,count=0;
+
+    printf("\nEnter number to check prime or not : ");
+    scanf("%d",&x);
+
+    for(i=2;i<=x/2;i++)
+    {
+        if(x%i==0)
+        {
+          count++;
+          break;
+        }
+    }
+
+    if(count)
+     printf("\nNumber is not prime ");
+     else
+      printf("\nNumber is prime ");    
+
+    return 0;
+}
