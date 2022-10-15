69eec7415dcbf9ddb3789570f977daa3c0ade1c1
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-16 02:02:27 +0530

    Assingment6Qn9 updated

--- Assignment 6/Assignment6Qn9.c
+++ Assignment 6/Assignment6Qn9.c
@@ -6,6 +6,7 @@ int main()
 {
     int a,b,i,j,d,lcm=1,x,y,count;
 
+    //Input two number for LCM
     printf("\nEnter two number to find their LCM : ");
     scanf("%d",&a);
     scanf("%d",&b);
@@ -15,14 +16,15 @@ int main()
     y=b;
 
     if(a>b)
-     d=b;
+     d=b;     //Smaller number stored in 'd' variable which will later will be used in loop
     else
      d=a;
 
-    for(i=2;i<=d;i++)
+    for(i=2;i<=d;i++)   //Here we are running loop to divide both input number by every prime factor starting from 2 till the smallest number
     {
         count = 0;
 
+        //Loop to check prime number
         for(j=2;j<=i/2;j++)
         {
           if(i%j==0)
@@ -31,19 +33,23 @@ int main()
             break;
           }
         }
+
+        //Only Prime number will enter this if block
         if(count==0)
         {
-            while((a%i==0)&&(b%i==0))
+            //O
+            while((a%i==0)&&(b%i==0))  //Here 'i' will divide both given number until it is not further divisible
             {
                 lcm = lcm*i;
-                a = a/i;
-                b = b/i;
-                d=a;               
+                a = a/i;      //If 'a' and 'b' is divisible by 'i', then we will modify a and b as it happens during LCM
+                b = b/i;      //ex : Say 12 and 24 is given number then a = 12/2 = 6 and b = 24/2 =12
+                d=a;          //New value of 'a' & 'b' will be 6 and 12 . Again it will check if 'a'&'b' is further divisible by 2 or not.  
             }
         }
     }
-    lcm = (lcm*a)*b;
+    lcm = (lcm*a)*b;   //Here calculating LCM by multiplying 'a' and 'b' that is the last two factor left which can't be divisible further 
 
+    //Displaying LCM
     printf("\nLCM of %d and %d is : %d ", x,y,lcm);
 
     return 0;
