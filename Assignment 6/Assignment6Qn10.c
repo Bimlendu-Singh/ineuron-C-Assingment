bd3093d2b9f97dd85fe80b8b9759bf408d18114b
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-17 13:11:00 +0530

    Assingment6Qn10 updated

--- Assignment 6/Assignment6Qn10.c
+++ Assignment 6/Assignment6Qn10.c
@@ -6,13 +6,16 @@ int main()
 {
     int n,i,x,y,count = 0,num=0;
 
+    //Input
     printf("\nEnter number : ");
     scanf("%d",&n);
-    y=n;
+    y=n;  //Storing the value of 'n' to 'y' so we don't loose the original number
    
+    //Loop to count the digit of the given number
     for(i=n;i!=0;i/=10)
      count++;
 
+    //Loop to reverse the entered number
     for(i=1;i<=count;i++)
     {
         x = n%10;
@@ -20,6 +23,7 @@ int main()
         n/=10;
     }
 
+    //Display
     printf("\nReverse of %d is %d\n", y,num);
 
 
