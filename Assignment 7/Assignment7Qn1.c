3f28b4b71539fb71126b6ffe4363bfcfc6f0acf1
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-22 16:43:44 +0530

    Assignment7Qn1 added

---
+++ Assignment 7/Assignment7Qn1.c
@@ -0,0 +1,32 @@
+//1. Write a program to find the Nth term of the Fibonnaci series.
+
+#include<stdio.h>
+
+int main()
+{
+   int res = 1,pos,i,prev=0,new=1;
+
+   //Position input 
+   printf("\nEnter the position : ");
+   scanf("%d",&pos);
+
+   if(pos==1)        //Fibonacci series 0,1,1,2,3,5,8,13,21,34,55,89,144...
+    res=0;
+  
+   else if(pos>3)
+    {
+      res = 0;  
+
+     for(i=1;i<pos-1;i++)
+     {
+       res = new + prev;    //Here, 'new' and 'prev' variable to store last and 2nd last digit so we can add them and store result in 'res' variable
+       prev = new;
+       new = res; 
+     }
+    }
+
+   printf("\nAt position %d of the Fibonnaci series is : %d\n",pos,res);
+
+   return 0;
+
+}
