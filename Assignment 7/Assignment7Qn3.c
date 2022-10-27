8048e89a333cdcb8cfb6f6a25b43dc45db345513
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-27 17:13:21 +0530

    Assignment7Qn3 added

--- Assignment 7/Assignment7Qn3.c
+++ Assignment 7/Assignment7Qn3.c
@@ -1,23 +1,19 @@
-//2. Write a program to print first N terms of Fibonacci series
+//3. Write a program to check whether a given number is there in the Fibonacci series or not.
 
 #include<stdio.h>
 
 int main()
 {
-   int res,pos,i,prev=0,new=1,n,j;
+   int res=-1,pos,i,prev=0,new=1,x,j,flag=0;
 
    //Fibonacci series 0,1,1,2,3,5,8,13,21,34,55,89,144...
 
    //Position input 
-   printf("\nEnter the nth term : ");
-   scanf("%d",&n);
+   printf("\nEnter the number to check whether it is in fibonacci series or not : ");
+   scanf("%d",&x);
 
-   printf("\nFibonnaci series till %dth term are : ",n);
-
-   //Loop to print fibonnaci series till nth term
-   for(i=1;i<=n;i++)
+   for(pos=1;x>res;pos++)
    {
-      pos = i; 
 
       if(pos==1)        
        res=0;
@@ -38,9 +34,18 @@ int main()
            new = res; 
          }
         }
-        printf(" %d ",res);   //Printing the finbonacci number each time
+
+        if(res==x)
+        {
+            flag++;
+            break;
+        }
    }
-   
+
+   if(flag)
+    printf("\n%d is in fibonacci series \n",x);
+   else
+    printf("\n%d is NOT in fibonacci series \n",x);
 
    return 0;
 
