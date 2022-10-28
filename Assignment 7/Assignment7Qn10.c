0f25c6759dfb2e25d2eddab18fcbf80f86ee6ea7
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2022-10-28 23:43:37 +0530

    Assignment7Qn9 and 10 updated

--- Assignment 7/Assignment7Qn10.c
+++ Assignment 7/Assignment7Qn10.c
@@ -8,19 +8,22 @@ int main()
 
     printf("\n Armstrong number under 1000 are : ");
 
+   //Loop to find armstrong numbe under 1000
    for(i=1;i<=1000;i++)
    {
      x = i;
      count = 0;
      sum = 0;
 
+    //Loop counting number of digits in the given number
      while(x!=0)
      {
       count++;
       x = x/10;
      }
-     x = i;
+     x = i;  //restoring original value of x
 
+    //Loop to check whether the 'i' is armstrong number or not.
      for(j=1;j<=count;j++)
      {
       res = 1;
@@ -31,7 +34,8 @@ int main()
       sum = sum + res;
       x = x/10;
      }
-
+    
+     //Printing armstrong number only
      if(sum==i)
       printf(" %d ",sum);
    } 
--- Assignment 7/Assignment7Qn9.c
+++ Assignment 7/Assignment7Qn9.c
@@ -1,35 +1,42 @@
 //9. Write a program to check whether a given number is an Armstrong number or not
 
+// In numerical number theory, the Armstrong number definition is the number in any given number base,
+//  which forms the total of the same number, when each of its digits is raised to the power of the number of digits in the number.
+
 #include<stdio.h>
 
 int main()
 {
-   int x,rem,sum=0,num,count=0,res,i,j;
+   int x,rem,sum=0,num,count=0,res,i,j;    //
 
+   //Input
    printf("\nEnter a number to check armstrong or not : ");
    scanf("%d",&x);
 
    num = x;
 
+   //Loop to count number of digits
    while(x!=0)
    {
      count++;
      x = x/10;
    }
-   
+   //Restoring the original value of x
    x = num;
 
+   //Loop to check armstrong number
    for(i=1;i<=count;i++)
    {
      res = 1;
      rem = x%10;
+     //Loop multiplying the remainder count times(Where,'count' is the count of number of digits in the given number)
      for(j=1;j<=count;j++)
       res = res*rem;
 
      sum = sum + res;
      x = x/10;
    }
-
+   //Display
    if(sum==num)
     printf("%d is an armstrong number",num);
    else
